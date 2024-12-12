f11aae42e03d03705526160d043c39523c502117
qianchenyimeng <xiaojin3642230@163.com>
2024-12-12 15:51:11 +0800

    Update EurekaApplication.java
    添加更新时间

--- eureka-server/src/main/java/com/wwj/EurekaApplication.java
+++ eureka-server/src/main/java/com/wwj/EurekaApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * 创建人：wuwenjin
  * 创建时间：2024/12/3
  * 修改人：
- * 修改时间：
+ * 修改时间：2024年12月12日15:53:16
  * 修改备注：上传到github上
  *
  * @version 1.0
